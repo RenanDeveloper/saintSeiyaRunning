1f02bb1a5524665287998ca94a2e3d160e068e89
RenanDeveloper <renantameirao@gmail.com>
2022-06-26 20:15:13 -0300

    inclusão da logo, do Score e lógica de incremento do score

--- js/script.js
+++ js/script.js
@@ -1,32 +1,41 @@
 const seiya = document.querySelector(".seiya-action");
 const box = document.querySelector(".box-gold");
+const score = document.querySelector("#score-value");
+var pontos = 8;
+score.textContent = pontos;
 
 const jump = () => {
   seiya.classList.add("jump-seiya");
+
 
   setTimeout(() => {
     seiya.classList.remove("jump-seiya");
   }, 500);
 };
 
+
+
 const loopGame = setInterval(() => {
 
   const boxPosition = box.offsetLeft;
-  const seiyaPosition = +window
-    .getComputedStyle(seiya)
-    .bottom.replace("px", "");
+  const seiyaPosition = +window.getComputedStyle(seiya).bottom.replace("px", "");
+    pontos += 1;
+    score.textContent = pontos;
 
-  if (boxPosition <= 120 && boxPosition > 0 && seiyaPosition < 80) {
-    box.style.animation = "none";
-    box.style.left = `${boxPosition}px`;
+    if (boxPosition <= 120 && boxPosition > 0 && seiyaPosition < 80) {
+      box.style.animation = "none";
+      box.style.left = `${boxPosition}px`;
 
-    seiya.style.animation = "none";
-    seiya.style.bottom = `${seiyaPosition}px`;
+      seiya.style.animation = "none";
+      seiya.style.bottom = `${seiyaPosition}px`;
 
-    seiya.src = "./img/seiya-fail.gif";
-
-    clearInterval(loopGame);
-  }
+      seiya.src = "./img/seiya-fail.gif";
+      
+      clearInterval(loopGame);
+    }
 }, 10);
 
+
 document.addEventListener("keydown", jump);
+
+
